70b8194faa596d3bf7c57eef7ffe2af0b388b3c1
dannysongyd <dannysongyd@gmail.com>
2021-12-14 17:49:42 -0500

    part2

--- run.c
+++ run.c
@@ -4,7 +4,48 @@
 #include <unistd.h>
 #include <limits.h>
 #include <string.h>
-#include "util.h"
+
+void readUtil(char *filename, size_t block_size, size_t block_count)
+{
+    int fd;
+    fd = open(filename, O_RDONLY);
+    if (fd < 0)
+    {
+        perror("r1");
+        exit(1);
+    }
+
+    unsigned int xor = 0;
+
+    unsigned int *buf = malloc(block_size);
+
+    // read
+    for (int i = 0; i < block_count; i++)
+    {
+        ssize_t read_size = read(fd, buf, block_size);
+
+        if (read_size < 0)
+        {
+            perror("r3");
+            exit(1);
+        }
+        ssize_t numOfIntSize = read_size / sizeof(int);
+        for (ssize_t j = 0; j < numOfIntSize; j++)
+        {
+            xor ^= buf[j];
+        }
+
+        // printf("%zu \n", read_size); // DELETE
+    }
+
+    // free
+    free(buf);
+
+    //print xor
+    printf("xor is %08x \n", xor);
+
+    close(fd);
+}
 
 unsigned int xorbuf(unsigned int *buffer, int size)
 {
--- run2.c
+++ run2.c
@@ -4,20 +4,62 @@
 #include <unistd.h>
 #include <limits.h>
 #include <string.h>
-#include "util.h"
 #include <time.h>
 
+void readUtil(char *filename, size_t block_size, size_t block_count)
+{
+    int fd;
+    fd = open(filename, O_RDONLY);
+    if (fd < 0)
+    {
+        perror("r1");
+        exit(1);
+    }
+
+    unsigned int xor = 0;
+
+    unsigned int *buf = malloc(block_size);
+
+    // read
+    for (int i = 0; i < block_count; i++)
+    {
+        ssize_t read_size = read(fd, buf, block_size);
+
+        if (read_size < 0)
+        {
+            perror("r3");
+            exit(1);
+        }
+        ssize_t numOfIntSize = read_size / sizeof(int);
+        for (ssize_t j = 0; j < numOfIntSize; j++)
+        {
+            xor ^= buf[j];
+        }
+
+        // printf("%zu \n", read_size); // DELETE
+    }
+
+    // free
+    free(buf);
+
+    //print xor
+    printf("xor is %08x \n", xor);
+
+    close(fd);
+}
 double countTime(char *filename, size_t block_size, size_t block_count)
 {
 
     // timer start time
     clock_t begin;
+    clock_t end;
     double time_spend;
-    unsigned int i;
-    begin = clock();
 
+    begin = clock();
     readUtil(filename, block_size, block_count);
-    time_spend = (double)(clock() - begin) / CLOCKS_PER_SEC;
+    end = clock();
+
+    time_spend = (double)(end - begin) / CLOCKS_PER_SEC;
 
     return time_spend;
 }
@@ -30,16 +72,18 @@ int main(int argc, char *argv[])
     filename = argv[1];
     sscanf(argv[2], "%lu", &block_size);
 
-    size_t block_count = 1;
+    size_t block_count = 1.0;
     double time = 0;
 
     // loop until runtime falls between 5 - 15 seconds
     while (time < 5)
     {
-        double time = countTime(filename, block_size, block_count);
-        block_count *= 2;
+        printf("time: %f \n", time);
+        block_count *= 2.0;
+        time = countTime(filename, block_size, block_count);
     }
-    printf("%f", time);
+    // printf("time: %f \n", time);
+    printf("given block_size %zu, our program can read block_count: %zu in %f seconds \n", block_size, block_count, time);
 
     exit(0);
 }
