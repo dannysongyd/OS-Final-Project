8a6c82139e0c5815b4f596fcac5d2b50471c92ee
dannysongyd <dannysongyd@gmail.com>
2021-12-15 20:40:47 -0500

    script for part3

---
+++ part3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -e "Running experiment for part 3 \n"
+
+for (( i=4; i < 134217728 ; i*=2 )); do
+    echo "block size: $i" 
+    ./run2 temp_30GB_file $i
+    echo -e "----------------------- \n"
+done
