baf55413ea4007e6896fe56ed3822b4d38fc141c
dannysongyd <dannysongyd@gmail.com>
2021-12-15 21:13:08 -0500

    fix script

--- part3.sh
+++ part3.sh
@@ -2,7 +2,7 @@
 
 echo -e "Running experiment for part 3 \n"
 
-for (( i=4; i < 134217728 ; i*=2 )); do
+for (( i=4; i <= 134217728 ; i*=2 )); do
     echo "block size: $i" 
     ./run2 temp_30GB_file $i
     echo -e "----------------------- \n"
