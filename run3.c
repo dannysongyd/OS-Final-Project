c975c8f2d5d5e38d46775caae9d36149c2d7c351
Wenbo Nan <wenbonan@Wenbos-MBP.lan>
2021-12-16 03:05:08 -0500

    Simplified run3.c

--- run3.c
+++ run3.c
@@ -49,9 +49,7 @@ double countTime(char *filename, size_t block_size, size_t block_count)
     readUtil(filename, block_size, block_count);
     end = clock();
 
-    time_spend = (double)(end - begin) / CLOCKS_PER_SEC;
-
-    return time_spend;
+    return (double)(end - begin) / CLOCKS_PER_SEC;
 }
 
 // * ./run <filename> [-r|-w] <block_size> <block_count>
@@ -59,57 +57,21 @@ int main(int argc, char *argv[])
 {
 
     char *filename;
-    char *mode;
     size_t block_size;
     size_t block_count;
     int fd;
     double runningTime;
 
     filename = argv[1];
-    mode = argv[2];
 
-    sscanf(argv[3], "%lu", &block_size);
-    sscanf(argv[4], "%lu", &block_count);
+    sscanf(argv[2], "%lu", &block_size);
+    sscanf(argv[3], "%lu", &block_count);
 
-    if (strcmp(mode, "-r") == 0)
-    {
-        // readUtil(filename, block_size, block_count);
-        runningTime = countTime(filename, block_size, block_count);
-        printf("Given block_size %zu (%f MB), our program can read block_count: %zu in %f seconds \n", block_size, (block_size / 1024.0 / 1024.0), block_count, runningTime);
-        printf("File size read %zu bytes (%f GB) \n", block_size * block_count, (block_size * block_count / (1024.0 * 1024.0 * 1024.0)));
-        printf("Reading speed is: %f Mib/s \n", (block_size * block_count) / (1024.0 * 1024.0 * runningTime));
-    }
-    else if (strcmp(mode, "-w") == 0)
-    {
-        fd = open(filename, O_CREAT | O_RDWR | O_APPEND, S_IRUSR | S_IWUSR);
-        if (fd < 0)
-        {
-            perror("r1");
-            exit(1);
-        }
-
-        unsigned int *buf = malloc(block_size);
-
-        for (int i = 0; i < block_count; i++)
-        {
-            ssize_t write_size = write(fd, buf, block_size);
-            if (write_size < 0)
-            {
-                perror("r3");
-                exit(1);
-            }
-
-            printf("%zu \n", write_size); // DELETE
-        }
-
-        free(buf);
-
-        close(fd);
-    }
-    else
-    {
-        printf("%s\n", "Invalid argument");
-    }
+    // readUtil(filename, block_size, block_count);
+    runningTime = countTime(filename, block_size, block_count);
+    printf("Given block_size %zu (%f MB), our program can read block_count: %zu in %f seconds \n", block_size, (block_size / 1024.0 / 1024.0), block_count, runningTime);
+    printf("File size read %zu bytes (%f GB) \n", block_size * block_count, (block_size * block_count / (1024.0 * 1024.0 * 1024.0)));
+    printf("Reading speed is: %f Mib/s \n", (block_size * block_count) / (1024.0 * 1024.0 * runningTime));
 
     exit(0);
 }
